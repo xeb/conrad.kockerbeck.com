9cf24e953ba1e2d2bcc9acc467f52084f7b96b31
Mark Kockerbeck <xeb@hey.com>
2024-07-29 20:57:16 -0700

    Dynamic videos!

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_from_directory, request
 import os
+import yaml
 
 app = Flask(__name__)
 
@@ -8,9 +9,28 @@ app = Flask(__name__)
 app.template_folder = 'templates'
 app.static_folder = 'static'
 
+def get_vids_data():
+    # Get the directory of the current script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Construct the path to the data.yaml file
+    yaml_path = os.path.join(current_dir, 'static', 'vids', 'data.yaml')
+    
+    # Read and parse the YAML file
+    try:
+        with open(yaml_path, 'r') as file:
+            data = yaml.safe_load(file)
+        return data["videos"]
+    except FileNotFoundError:
+        print(f"Error: The file {yaml_path} was not found.")
+        return None
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}")
+        return None
+
 @app.route('/')
 def index():
-    return render_template('index.html', title="Conrad's Birthday Celebration")
+    return render_template('index.html', videos=get_vids_data())
 
 @app.route('/video/<name>')
 def serve_video(name):
---
+++ upload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -avz --update static/ xeb@gal.xeb.ai:/home/xeb/projects/conrad.kockerbeck.com/static/
